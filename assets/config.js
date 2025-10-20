fb4bbacc7c2e3b604c7bbbaa6eba36c67a665f77
Joseph Havens <89954591+JHavens2187@users.noreply.github.com>
2025-10-20 13:18:29 -0500

    Update config.js with personal information and links

--- assets/config.js
+++ assets/config.js
@@ -1,47 +1,27 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Joseph Havens - Links",
+  "Name": "Joseph Havens",
+  "Description": "Physics & Astronomy Student at the University of Kansas",
   "links": [
     {
-      "Title": "Website",
-      "URL": "http://www.harindu.dev",
-      "icon_classes": "fas fa-link",
-    },
-    {
-      "Title": "Github",
-      "URL": "https://github.com/harindulk",
+      "Title": "GitHub",
+      "URL": "https://github.com/JHavens2187",
       "icon_classes": "fab fa-github",
     },
     {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
-      "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
+      "Title": "View My CV",
+      "URL": "assets/CV.WIP.pdf",
+      "icon_classes": "fas fa-file-alt",
     },
     {
       "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
+      "URL": "https://www.linkedin.com/in/joseph-havens-1b3a73348/",
       "icon_classes": "fab fa-linkedin"
     },
     {
-      "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
+      "Title": "Bluesky",
+      "URL": "https://bsky.app/profile/cosmicjoe.bsky.social",
+      "icon_classes": "fas fa-comment"
     },
   ]
 }
